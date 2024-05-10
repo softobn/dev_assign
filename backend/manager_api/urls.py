46d30059f9ad3695725b0a1dc284c247aa848071
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-05-10 11:38:40 +0600

    feat: task api created

--- backend/manager_api/urls.py
+++ backend/manager_api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views.project import ManagerCreateProjectView
 from .views.project import ManagerUpdateProjectView
+from .views.task import ManagerCreateTaskView
+from .views.task import ManagerUpdateTaskView
 
 
 urlpatterns = [
@@ -16,5 +18,17 @@ urlpatterns = [
         route="project-update/",
         view=ManagerUpdateProjectView.as_view(),
         name="manager_project_update",
+    ),
+    # http://localhost:8000/api/v1/manager/task-create/
+    path(
+        route="task-create/",
+        view=ManagerCreateTaskView.as_view(),
+        name="manager_task_create",
+    ),
+    # http://localhost:8000/api/v1/manager/task-update/
+    path(
+        route="task-update/",
+        view=ManagerUpdateTaskView.as_view(),
+        name="manager_task_update",
     ),
 ]
---
+++ backend/manager_api/views/task.py
@@ -0,0 +1,38 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from task.models import TaskModel
+from utils.custom_permission import IsManager
+from ..serializers.task import ManagerCreateTaskSerializer
+
+
+class ManagerCreateTaskView(APIView):
+    permission_classes = [IsManager]
+
+    def post(self, request):
+        serializer = ManagerCreateTaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("success")
+        
+        return Response("unsuccess")
+
+
+class ManagerUpdateTaskView(APIView):
+    permission_classes = [IsManager]
+
+    def validate_parameter(self, task_id):
+        return task_id is not None
+
+    def patch(self, request):
+        task_id = request.data.get("task_id")
+        if self.validate_parameter(task_id) is True:
+            task = get_object_or_404(TaskModel, id=task_id)
+            serializer = ManagerCreateTaskSerializer(instance=task, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response("success")
+        
+        return Response("unsuccess")
