4fbc2580196514400b76f39549d088685227afdd
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-05-13 11:01:50 +0600

    chore: user admin interface changed

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
 from user.models import UserAccount
 
 
 class UserAccountAdmin(admin.ModelAdmin):
+    def display_picture(self, obj):
+        return mark_safe('<img src="%s" style="max-width:100px; max-height:100px;" />' % obj.profile_picture)
+    display_picture.allow_tags = True
+    display_picture.short_description = 'Picture'
+
     list_display = (
         "phone_number",
         "email",
         "gender",
         "area",
         "total_project",
+        "display_picture",
         "is_developer",
         "is_manager",
         "is_active",
