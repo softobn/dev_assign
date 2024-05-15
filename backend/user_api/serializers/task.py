7b4f7c55ceabb6387332d990c8645614fad1e721
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-05-15 08:07:58 +0600

    chore: task api modified

--- backend/user_api/serializers/task.py
+++ backend/user_api/serializers/task.py
@@ -1,18 +1,29 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from task.models import TaskModel
 
 
-class TaskListSerializer(ModelSerializer):
+class TaskListSerializer(serializers.ModelSerializer):
+    project_title = serializers.CharField(source='project.title', read_only=True)
+    developer_first_name = serializers.CharField(source='developer.first_name', read_only=True)
+    developer_last_name = serializers.CharField(source='developer.last_name', read_only=True)
+    developer_email = serializers.CharField(source='developer.email', read_only=True)
+    developer_profile_picture = serializers.CharField(source='developer.profile_picture', read_only=True)
     class Meta:
         model = TaskModel
         fields = [
             "id",
             "title",
             "project",
+            "project_title",
             "developer",
+            "developer_first_name",
+            "developer_last_name",
+            "developer_email",
+            "developer_profile_picture",
             "description",
             "requirements",
             "deadline",
             "is_complete",
+            "is_active",
             ]
